6bf39b0c4a242123d9b0ab2d91a09d218f0bab87
vega jafri <giantwhitedeer@gmail.com>
2025-10-23 03:12:04 -0400

    asdssdfdfgdfgdfgdfgdfgdfg

--- systems-prototype-04/Assets/FootstepSound.cs
+++ systems-prototype-04/Assets/FootstepSound.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class FootstepSound : MonoBehaviour
 {
+    private AudioSource walk;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        walk = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey("w") || Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("d"))
+        {
+            Debug.Log("buttons are being pressed");
+            if (!walk.isPlaying) 
+            {
+                walk.Play();
+            }
+        }
+        else
+        {
+            if (walk.isPlaying)
+            {
+                walk.Pause();
+            }
+        }
     }
 }
